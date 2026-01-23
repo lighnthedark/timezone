ede6e81655c6fe98e3edd36863de1ddcdb06f5fc
Spencer TGreen <spencerTgreen@yahoo.com>
2026-01-23 10:56:05 -0600

    Fixed port binding

--- app.py
+++ app.py
@@ -161,4 +161,6 @@ def convert_currency():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5002)
+    import os
+    port = int(os.environ.get('PORT', 5002))
+    app.run(host='0.0.0.0', port=port, debug=True)
