8a90fdc3125e9e04bf022fbd770af5e8c1214d72
Spencer TGreen <spencerTgreen@yahoo.com>
2026-03-05 13:31:20 -0600

    index.html

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,5 @@
-for number in range(1, 31):
-    if number % 3 == 0 and number % 5 == 0:
-        print("FizzBuzz")
-    elif number % 3 == 0:
-        print("Fizz")
-    elif number % 5 == 0:
-        print("Buzz")
-    else:
-        print(number)
+for number in range(1, 21):
+    if number % 2 == 0:
+        print("Even")
+    elif number % 2 == 1
+        print("Odd")
